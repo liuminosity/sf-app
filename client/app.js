82781977dfe351cdb07f78639ced613ca34edac6
Liuminosity <liu.stanford@gmail.com>
2015-10-24 13:17:14 -0700

    [feat] (general): set up file structure and base modules

---
+++ client/app.js
@@ -0,0 +1,23 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Link = ReactRouter.Link;
+var RouteHandler = ReactRouter.RouteHandler;
+var defaultRoute = ReactRouter.DefaultRoute;
+
+var App = React.createClass({
+
+  componentWillMount: function componentWillMount() {
+    var _this = this;
+    $.ajax({
+      
+    });
+  },
+
+  render: function render() {
+    return (<div> Hello World </div>);
+  },
+})
+
+
